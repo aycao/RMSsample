d9d781d19296eaa96be32defab4031b517ef0c41
cyssndy <cyssndy@hotmail.com>
2015-12-07 18:28:36 +0000

    add php config file

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    'host' => 'us-cdbr-azure-central-a.cloudapp.net',
+    'username' => 'b9d90ee5a6af83',
+    'password' => 'fdd5c6a3ff3c4ee',
+    'dbname' => 'RMS',
+);
+
+//mysql --host=us-cdbr-azure-central-a.cloudapp.net --user=b9d90ee5a6af83 --password=fdd5c6a3ff3c4ee RMS
+?>
